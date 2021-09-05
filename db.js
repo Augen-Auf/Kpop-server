294947c5198d2564127165e2f71207f9c6a7b9e8
Augen-Auf <an_lesiv@mail.ru>
2021-09-05 15:55:16 +0500

    update package.json

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const {Sequelize} = require('sequelize');
 
 const devConnectionString = `postgres://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`
-const prodConnectionString =  process.env.DATABASE_URL
+const prodConnectionString =  process.env.DATABASE_URL + "?sslmode=require"
 module.exports = new Sequelize(process.env.NODE_ENV === "production" ? prodConnectionString : devConnectionString);
